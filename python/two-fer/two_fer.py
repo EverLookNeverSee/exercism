ea1e70901238739b5d37b26267b077254d2b7a1c
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-03-22 15:57:26 +0430

    Solving problem using another default parameter value

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,5 @@
-def two_fer(name=None) -> str:
-    if name is None:
+def two_fer(name="") -> str:
+    if name == "":
         return "One for you, one for me."
     else:
         return f"One for {name}, one for me."
