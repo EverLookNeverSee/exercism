4bd95e88acd79b3d13edb3ab4e8e13a3081a8dd6
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-03-26 19:56:16 +0430

    Solving the problem

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
-def convert(number):
-    pass
+def convert(number) -> str:
+    drops = ""
+    if number % 3 == 0:
+        drops += "Pling"
+    if number % 5 == 0:
+        drops += "Plang"
+    if number % 7 == 0:
+        drops += "Plong"
+    if not drops:
+        drops = str(number)
+    return drops
