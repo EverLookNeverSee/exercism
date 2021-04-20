32c13985c3aa6920137f926ceac0e683380293a4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-20 22:28:16 +0430

    Implementing init method

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,6 +1,8 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.rows = [[int(n) for n in row.split()]
+                     for row in matrix_string.split("\n")]
+        self.columns = [list(elements) for elements in zip(*self.rows)]
 
     def row(self, index):
         pass
