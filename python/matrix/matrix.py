d274eb02ef8eb7fddf0aa951a2a25fc8645196e6
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-21 20:26:57 +0430

    Adding typo hints in order to make code more cleaner

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,13 +2,13 @@ from typing import List
 
 
 class Matrix:
-    def __init__(self, matrix_string):
+    def __init__(self, matrix_string: str) -> None:
         self.rows = [[int(n) for n in row.split(" ")]
                      for row in matrix_string.split("\n")]
         self.columns = [list(elements) for elements in zip(*self.rows)]
 
-    def row(self, index):
+    def row(self, index: int) -> List:
         return self.rows[index - 1]
 
-    def column(self, index):
+    def column(self, index: int) -> List:
         return self.columns[index - 1]
