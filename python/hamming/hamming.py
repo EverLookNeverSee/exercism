0fec7acde5c4e98db31dee298a0a95cc9df7cc36
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-29 20:22:35 +0430

    Implementing the rest of the algorithm

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,3 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Length of sequences must be equal")
 
+    return sum(item_1 != item_2 for item_1, item_2 in zip(strand_a, strand_b))
