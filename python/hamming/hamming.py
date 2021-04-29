cd3c9fd7b89c1ad425369b7f79a6ed501713667b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-29 20:34:45 +0430

    Adding type hints and function docstring

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,10 @@
-def distance(strand_a, strand_b):
+def distance(strand_a: str, strand_b: str) -> int:
+    """
+    Calculating hamming distance
+    :param strand_a: str, DNA sequence
+    :param strand_b: str, DNA sequence
+    :return: int, number of differences
+    """
     if len(strand_a) != len(strand_b):
         raise ValueError("Length of sequences must be equal")
 
